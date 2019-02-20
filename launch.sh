b1b15983dd58a7c9c8e39f4ef14b057bb1ca7fb2
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-02-20 17:52:56 +0100

    Add port expose in launch.sh and usefull packages to the Dockerfile

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 IMAGE=dylanvanassche/coaco-docker:latest
 VOLUME=ros_mount
+PORT=11311
 
 echo "Shared volume will be mounted in: $HOME/$VOLUME"
 echo "Pulling Docker image: $IMAGE"
 docker pull $IMAGE
-docker run -t -i -v $HOME/$VOLUME:/root/$VOLUME $IMAGE /bin/bash
+docker run -t -i -v $HOME/$VOLUME:/root/$VOLUME -p $PORT:$PORT $IMAGE /bin/bash
