2c2df190401b4798c31de7760c5c79181b8a1944
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-02-21 20:29:10 +0100

    Fixed Docker build

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN apt install -y vim iputils-ping net-tools wget
 
 # Setup the folder containing the catkin workspace
 RUN mkdir -p ~/catkin_ws/src
-RUN /bin/bash -c '. /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; catkin_make; source ./devel/setup.bash; roscore'
+RUN /bin/bash -c '. /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; catkin_make; source ./devel/setup.bash'
 
 # Python packages
 RUN pip3 install --upgrade pip setuptools
--- launch.sh
+++ launch.sh
@@ -6,5 +6,6 @@ PORT=11311
 echo "Shared volume will be mounted in: $HOME/$VOLUME"
 echo "Pulling Docker image: $IMAGE"
 echo "You should set the ROS_MASTER_URI to http://IP_OF_THIS_PC:11311 on ALL REMOTE devices! We assume that roscore is running in our Docker!"
+echo "Launching a roscore instance can be done using: roscore & (in the background)"
 docker pull $IMAGE
 docker run -t -i -v $HOME/$VOLUME:/root/$VOLUME -p $PORT:$PORT $IMAGE /bin/bash
